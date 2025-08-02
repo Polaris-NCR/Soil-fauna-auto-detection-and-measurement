f89319e873f6d1f8579bc9cc78785d03000454ae
Polaris-NCR <2401213501@stu.pku.edu.cn>
2025-08-02 14:28:27 +0800

    Seg.py

--- Seg.py
+++ Seg.py
@@ -11,14 +11,9 @@ def crop_image_and_pre(img_path, module, device, crop_size=64, stride=64, output
     raster = img.ReadAsArray()
     if raster.shape[0] == 4:
         raster = raster[:3]
-
     indices = np.where(raster[0] == 255)
     gray = utils.RGB_to_gray(raster, label_filter=True, savepath=None)
-    # musked_image = build_circle_musk(gray, savepath=None)
-
     h, w = raster.shape[1:]
-    # base_name = os.path.splitext(os.path.basename(img_path))[0]
-
     mask_arr = np.zeros_like(raster[0])
     out_arr = np.zeros_like(raster[0])
 
@@ -26,9 +21,7 @@ def crop_image_and_pre(img_path, module, device, crop_size=64, stride=64, output
         for x in range(0, w, stride):
             if (y + crop_size) > h or (x + crop_size) > w:
                 continue
-
             img_crop = raster[:, y:y + crop_size, x:x + crop_size]
-
             tensor = torch.tensor(img_crop).float().unsqueeze(0).to(device)
             output = module(tensor)
             pred_mask = (output > 0.5).float().squeeze().cpu().numpy()
@@ -42,7 +35,6 @@ def crop_image_and_pre(img_path, module, device, crop_size=64, stride=64, output
     radius = np.sqrt(np.power(center[0], 2) + np.power((center[1] - index), 2))
     radius = math.floor(radius) - 100
     rr, cc = disk(center, radius, shape=(height, width))
-
     mask = np.zeros((height, width), dtype=bool)
     mask[rr, cc] = True
     out_arr[mask] = mask_arr[mask]
